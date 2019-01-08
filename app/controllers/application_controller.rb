1fbc69aa2651fe038c749decf74ae9998521b09a
lsamano <leizl.samano@gmail.com>
2019-01-08 17:56:50 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,49 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  set :method_override, true
+  # this is index
+  get '/recipes' do
+    @all_recipes = Recipe.all
+    # binding.pry
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  #this is show
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    # binding.pry
+    erb :show
+  end
+
+  #this is delete
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect "/recipes"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  post '/recipes' do
+    recipe = Recipe.new(params)
+    recipe.save
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(name: params[:name],ingredients: params[:ingredients],cook_time: params[:cook_time])
+    redirect "/recipes/#{params[:id]}"
+  end
+
 
 end
---
+++ db/migrate/20190108201006_make_cook_time_column_accept_strings_in_recipes.rb
@@ -0,0 +1,5 @@
+class MakeCookTimeColumnAcceptStringsInRecipes < ActiveRecord::Migration
+  def change
+    change_column :recipes, :cook_time, :string
+  end
+end
